686ace399efdfa35167e496a674a8f1e883216d1
toa.web.dev <toa.wev.dev@gmail.com>
2025-01-18 18:53:28 +0900

    Add main components, footer, global styles

---
+++ src/components/Main/ProfileImage.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+
+// 자신이 원하는 프로필 이미지 링크로 설정해주세요.
+const PROFILE_IMAGE_LINK =
+  '/icons/icon-512x512.png'
+
+const ProfileImageWrapper = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  border-radius: 50%;
+`
+
+const ProfileImage: FunctionComponent = function () {
+  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+}
+
+export default ProfileImage
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,31 @@
 import React, { FunctionComponent } from 'react'
-import { Link } from 'gatsby'
+import styled from '@emotion/styled'
+import GlobalStyle from 'components/Common/GlobalStyle'
+import Footer from 'components/Common/Footer'
+import CategoryList from 'components/Main/CategoryList'
+import Introduction from 'components/Main/Introduction'
+
+const CATEGORY_LIST = {
+    All: 5,
+    Web: 3,
+    Mobile: 2,
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`
 
 const IndexPage: FunctionComponent = function () {
     return (
-        <div>
-            <Link to="/info/">To Info</Link>
-        </div>
+        <Container>
+            <GlobalStyle />
+            <Introduction />
+            <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
+            <Footer />
+        </Container>
     )
-
 }
 
 export default IndexPage
