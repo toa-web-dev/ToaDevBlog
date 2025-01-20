9adf14f15f13981dc37fedcdd7cf2f647ba56cc1
gimHyoungSoo <gimhyoungsoo@gmail.com>
2025-01-20 14:43:01 +0900

    Add slug generation for MarkdownRemark nodes in gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,5 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: <https://www.gatsbyjs.com/docs/node-apis/>
- */
-
-// You can delete this file if you're not using it
-
 const path = require('path');
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
 // Setup Import Alias
 exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
@@ -22,4 +15,15 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
       },
     },
   });
+};
+
+// Generate a Slug Each Post Data
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode });
+
+    createNodeField({ node, name: 'slug', value: slug });
+  }
 };
