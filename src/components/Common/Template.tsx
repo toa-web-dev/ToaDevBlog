5092a7bd562b8793fe9ca593c7accf5508c5ee8b
gimHyoungSoo <gimhyoungsoo@gmail.com>
2025-01-21 18:16:03 +0900

    Assesibility: use sementic tag and html lang attribute

--- src/components/Common/Template.tsx
+++ src/components/Common/Template.tsx
@@ -12,7 +12,7 @@ type TemplateProps = {
   children: ReactNode
 }
 
-const Container = styled.div`
+const Container = styled.main`
   display: flex;
   flex-direction: column;
   height: 100vh;
@@ -47,6 +47,8 @@ const Template: FunctionComponent<TemplateProps> = function ({
         <meta name="twitter:image" content={image} />
         <meta name="twitter:site" content="@사용자이름" />
         <meta name="twitter:creator" content="@사용자이름" />
+
+        <html lang="ko" />
       </Helmet>
 
       <GlobalStyle />
