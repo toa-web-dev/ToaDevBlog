40698d55734f86f739fd3d406c86431563bdef9d
gimHyoungSoo <gimhyoungsoo@gmail.com>
2025-01-21 18:01:41 +0900

    feat: update site URL configuration and add sitemap and canonical URL plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,14 @@
+const SITE_URL = `https://google.com`
+
 module.exports = {
   siteMetadata: {
     title: `주니어 개발자의 개발 블로그`,
     description: `주니어 개발자로서의 저를 표현한 블로그입니다.`,
     author: `Hyun`,
-    siteUrl: 'https://google.com', // 배포 후 변경 예정
+    siteUrl: SITE_URL, // 배포 후 변경 예정
   },
   plugins: [
+    'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-typescript',
       options: {
@@ -67,6 +70,13 @@ module.exports = {
             },
           },
         ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-canonical-urls',
+      options: {
+        siteUrl: SITE_URL,
+        stripQueryString: true,
       },
     },
   ],
